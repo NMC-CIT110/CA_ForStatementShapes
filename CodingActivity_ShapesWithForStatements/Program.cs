a0f2e1788cf9df732b758b6b08c5f43564ef95fb
John Velis <john@johnvelis.com>
2016-08-31 16:00:46 -0400

    initial commit

---
+++ CodingActivity_ShapesWithForStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingActivity_ShapesWithForStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilledRectangle(5, 10);
+
+            Console.ReadKey();
+        }
+
+        static void FilledRectangle(int width, int length)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
